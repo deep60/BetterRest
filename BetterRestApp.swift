805bb398a55b3863606f2dc8959b2521fff82825
P Deepanshu <97436890+deep60@users.noreply.github.com>
2022-12-31 08:21:00 +0530

    Add files via upload

---
+++ BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by P Deepanshu on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
